c334731802f9435f5d7965810a8a733d220f05e4
boos4721 <3.1415926535boos@gmail.com>
2020-03-13 15:09:16 +0800

    ?: for gcp
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

---
+++ ssh.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+######################################################
+# Copyright (C) 2019 @Boos4721(Telegram and Github)  #
+#                                                    #
+# SPDX-License-Identifier: GPL-3.0-or-later          #
+#                                                    #
+######################################################
+echo "Patch Root User ...."
+sudo -i
+echo "Enable SSH ...."
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config; 
+echo "Patch Boos User...."
+adduser boos
+echo "Patch Boos To Root User…."
+chmod 777 /etc/sudoers
+echo "# boos ALL=(ALL) NOPASSWD: ALL" >>  /etc/sudoers
+chmod 440 /etc/sudoers
+echo "Done ...."
