aa1996d7f383154d207d936077e01f3f15956ff2
boos4721 <3.1415926535boos@gmail.com>
2020-03-13 15:18:58 +0800

    ?: root isnt working
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- ssh.sh
+++ ssh.sh
@@ -6,7 +6,7 @@
 #                                                    #
 ######################################################
 echo "Patch Root User ...."
-sudo -i
+sudo su
 echo "Enable SSH ...."
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config; 
