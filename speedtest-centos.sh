1eafb8bce6c47c114d1c971b04a4ca76b95d3628
boos4721 <3.1415926535boos@gmail.com>
2020-03-14 15:25:14 +0800

    Net: Speedtest
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

---
+++ speedtest-centos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+ wget https://bootstrap.pypa.io/get-pip.py --no-check-certificate
+ python get-pip.py
+ pip install speedtest-cli
+ speedetest
