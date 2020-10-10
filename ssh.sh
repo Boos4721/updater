874f8e5ae27490ae7df336ac2a6b07a983366ace
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2020-10-10 19:06:16 +0800

    ?

--- ssh.sh
+++ ssh.sh
@@ -6,12 +6,12 @@
 #                                                    #
 ######################################################
 echo "Enable SSH ...."
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+# sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config; 
+echo "AllowUsers root admin boos " >>  /etc/ssh/sshd_config
 echo "Patch Boos User...."
 sudo adduser boos && sudo passwd boos
 echo "Patch Boos To Root User…."
-chmod 777 /etc/sudoers
-echo "boos ALL=(ALL) NOPASSWD: ALL" >>  /etc/sudoers
-chmod 0440 /etc/sudoers
+usermod -aG sudo boos
+ /etc/init.d/ssh restart
 echo "Done ...."
