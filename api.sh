bc2e263b743da0b37031c557ece2b5b7aba8921c
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2021-03-21 14:38:26 +0800

    Create api.sh

---
+++ api.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+yum update
+yum install java-1.8.0-openjdk
+systemctl start firewalld
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+mkdir -p /www/api && cd /www/api
+yum install wget 
+wget https://hub.fastgit.org/ZimoLoveShuang/wisedu-unified-login-api/releases/download/v1.0.19/wisedu-unified-login-api-v1.0.jar
+nohup java -jar wisedu-unified-login-api-v1.0.jar --server.port=80>jinri.log 2>&1 &
+echo -e "Done"
